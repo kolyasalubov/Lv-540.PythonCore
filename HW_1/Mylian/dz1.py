3211b61c284a31a13295cd59db6b463c8c59c575
Nastia10 <72551257+Nastia10@users.noreply.github.com>
2020-10-09 20:04:05 +0300

    Update dz1.py

--- HW_1/Mylian/dz1.py
+++ HW_1/Mylian/dz1.py
@@ -1,18 +1,14 @@
-#a=12
-#b=8
-#print("a+b=", a+b)
-#print("a-b=", a-b)
-#print("a*b=", a*b)
-#print("a/b=", a/b)
-#print("a**b=", a**b)
-
-#name=input("What is your name?")
-#print("Hello,", name)
-#age=input("How old are you?")
-#print("Your age is", age)
-#live=input("Where do you live?")
-#print("You live in", live)
-
-a=input("enter_a")
-b=input("enter_b")
+a=12
+b=8
 print("a+b=", a+b)
+print("a-b=", a-b)
+print("a*b=", a*b)
+print("a/b=", a/b)
+print("a**b=", a**b)
+
+name=input("What is your name?")
+print("Hello,", name)
+age=input("How old are you?")
+print("Your age is", age)
+live=input("Where do you live?")
+print("You live in", live)
