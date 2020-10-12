fc1c214eb004c77f47c66da89596adffca2ab66b
Роман Сметаняк <romansmetanak@MacBook-Pro-Roman.local>
2020-10-12 21:56:13 +0300

    Smetaniak

---
+++ HW_1/Smetaniak/2.py
@@ -0,0 +1,7 @@
+a = float(input("ВВЕДИ : A (число а) - "))
+b = float(input("ВВЕДИ : B (число b) - " ))
+print ("a+b=", a+b)
+print ("a-b=", a-b)
+print ("a*b=", a*b)
+print ("a/b=", a/b)
+print ("a**b=", a**b)
