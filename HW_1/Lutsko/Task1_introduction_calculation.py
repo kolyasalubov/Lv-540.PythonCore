834204deae5f031db82612ca4c1b7dd0e6dc669e
Lutsko Viktoriia <vika.purdyk@gmail.com>
2020-10-13 19:45:19 +0300

    add resolved homework1

---
+++ HW_1/Lutsko/Task1_introduction_calculation.py
@@ -0,0 +1,10 @@
+#output_calculation
+a = 100
+b = 6
+print("a+b =", a+b)
+print("a-b =", a-b)
+print("a*b=", a*b)
+print("a/b=", a/b)
+print("a**b=", a**b)
+
+
---
+++ HW_1/Lutsko/Task1_introduction_output_questions.py
@@ -0,0 +1,7 @@
+#output_questions
+user_name = input("What is your name? ")
+user_old = input("How old are you? ")
+user_city = input("Where do you live? ")
+print("Hello,", user_name)
+print("Your age is", user_old)
+print("You live in", user_city)
