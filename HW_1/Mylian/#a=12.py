c54f9c6f7c8c310d4f95d4256ff060785dec492a
Nastia10 <nastiamylian1011@gmail.com>
2020-10-15 13:57:34 +0300

    add homework 1

---
+++ HW_1/Mylian/#a=12.py
@@ -0,0 +1,26 @@
+#a=12
+#b=8
+#print("a+b=", a+b)
+#print("a-b=", a-b)
+#print("a*b=", a*b)
+#print("a/b=", a/b)
+#print("a**b=", a**b)
+
+#name=input("What is your name?")
+#print("Hello,", name)
+#age=input("How old are you?")
+#print("Your age is", age)
+#live=input("Where do you live?")
+#print("You live in", live)
+
+#a=input("enter_a ")
+#b=input("enter_b ")
+#var=a+b
+#print("a+b=", var)
+
+#print(2+2)
+#var=2+2
+#print("4*2= ", var * 2)
+s = "gh"
+f = "ljunyo"
+print("s", s*3)
