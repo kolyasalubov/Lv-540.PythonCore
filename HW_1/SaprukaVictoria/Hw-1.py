b052de77fac0068e3f25744c1e252b3ffc55ee37
Victoriiaaa <sapruka77@gmail.com>
2020-10-16 20:45:40 +0300

    new add hm1

---
+++ HW_1/SaprukaVictoria/Hw-1.py
@@ -0,0 +1,4 @@
+GUESTION_1=input("What is your name? ")
+GUESTION_2=input("How old are you? ")
+GUESTION_3=input("Where do you live? ")
+print ( "Hello:  " , GUESTION_1 , "\nYour age is: ", GUESTION_2  ,"\nYou live in: ", GUESTION_3)
