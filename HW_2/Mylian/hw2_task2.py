2d67c821501bbfe0caa7e41ab92175e4ec4b8dd4
Nastia10 <72551257+Nastia10@users.noreply.github.com>
2020-10-17 14:48:07 +0300

    Update hw2_task2.py

--- HW_2/Mylian/hw2_task2.py
+++ HW_2/Mylian/hw2_task2.py
@@ -4,10 +4,21 @@ print("Reverse: ", data[: : -1])
 s_list=['5', '4', '9', '8']
 print("Sorted: ", sorted(s_list))
 
-n=5498
-#n=int(input("Число: "))
-a=n%10
-b=n%100//10
-c=n%1000//100
-d=n%10000//1000
-print("Добуток:", a*b*c*d)
+#n=5498
+##n=int(input("Число: "))
+#a=n%10
+#b=n%100//10
+#c=n%1000//100
+#d=n%10000//1000
+#print("Добуток:", a*b*c*d)
+
+n = 5498
+mult = 1
+
+while n > 0:
+    d = n % 10
+    
+    mult = mult * d
+    n = n // 10
+
+print("добуток:", mult)
