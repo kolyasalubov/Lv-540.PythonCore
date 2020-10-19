5cd6355d18b4a07dd2297c252735a10da0589f9d
Victoria Sapruka <sapruka77@gmail.com>
2020-10-19 19:49:21 +0300

    feat: added homework 1.2

---
+++ HW_1/SaprukaVictoria/Hw-1.2.py
@@ -0,0 +1,5 @@
+import random
+a=(random.randint(0, 1000))
+b=(random.randint(0, 5000))
+print("a=", a ,"\nb=", b)
+print ( "a+b=", a+b, "\na-b=",a-b , "\na*b=", a*b, "\na/b=", a/b, "\na**b=", a**b  )
