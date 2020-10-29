6b80c492fe41763a292e7b8d6f4d76647da46dd1
Yurii Zaverukha <niko2yurii@gmail.com>
2020-10-29 20:39:41 +0200

    add new folder CW_3 and new file classwork_Function

---
+++ CW_3/Zaverukha/classwork_Function.py
@@ -0,0 +1,54 @@
+# # Task_1
+
+# def average(smth):
+#     """This function finds average value """
+#     numbers = []
+#     number_0 = int(input("Enter the number of numbers - "))
+#     for av in range(number_0):
+#         numbers.append(int(input("Enter your number - ")))
+#     number_average = sum(numbers) / len(numbers)
+#     print(number_average)
+# average(True)
+# Task_2
+# def maximum (first_number, second_number):
+#     """This function finds biggest number"""
+#     if first_number > second_number:
+#         print ("First number", first_number, "is bigger")
+#     else:
+#         print ("Second number", second_number, "is bigger")
+#  maximum(int(input("Enter first number - ")), int(input("Enter second number - ")))
+# Task 3
+# def triangle():
+#     """This function finds area of triangle"""
+#     main_side = int(input("Enter length of main side - "))
+#     height = int(input("Enter length "))
+#     area = (main_side*height)/2
+#     print(area)
+# def rectangle():
+#     """This function finds area of rectangle"""
+#     first_side = int(input("Enter first side - "))
+#     second_side = int(input("Enter second side - "))
+#     area = first_side * second_side
+#     print(area)
+# def circle():
+#     """This funcrion finds area of circle"""
+#     radius = int(input("Enter radius of circle - "))
+#     PI = 3.14
+#     area = PI*radius**2
+#     print (area)
+# figure =input("Enter which figure area you look (triangle, rectangle, circle) - ")
+
+
+# if figure == "triangle":
+#     triangle()
+# if figure == "rectangle":
+#     rectangle()
+# if figure == "circle":
+#     circle()
+# Task 4
+
+# def length():
+#     text = input("Enter your text - ")
+#     length_text = len(text)
+#     print(length_text)
+# length()
