584ee9a24e3ae163e8df155426992c0a47470b34
Yurii Zaverukha <niko2yurii@gmail.com>
2020-10-29 19:24:04 +0200

    add new folder CW_2 and file CW_2.py

---
+++ CW_2/classwork_2.py
@@ -0,0 +1,24 @@
+# # Створити список цілих чисел і знайти найбільше та найменше значення   
+# list_1 = []
+# number_user = int(input("Enter number: "))
+# for value in range(number_user):
+#     list_1.append(int(input("Enter number: ")))
+# print("List - ", list_1)
+# Biggest_number = max(list_1)
+# Smallest_number = min(list_1)
+# print("Minimum - ", Smallest_number)
+# print("Maximum - ", Biggest_number)
+# # В інтервалі від 1 до 10 визначити числа парні, непарні
+# for number_first in range (1, 11):
+#     if number_first % 2 == 0:
+#         print(number_first, "- number is even")
+# for num in range(1, 11):
+#     if num % 3 == 0:
+#         print(num, "- number is odd")
+#Написати скрип перевірки логіна
+# login = input("Enter your login - ")
+# while login == "First":
+#     print("Hello, my User")
+#     break
+# else:
+#     print("You are not my User")
