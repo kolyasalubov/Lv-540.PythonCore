86ef850ad7a79ef0067123e79317eb60c1beb4e9
Victoria Sapruka <sapruka77@gmail.com>
2020-11-05 13:12:40 +0200

    feat: added Are_you_playing_Banjo?

---
+++ Homeworks/SaprukaVictoria/Are_you_playing_Banjo?.py
@@ -0,0 +1,6 @@
+def areYouPlayingBanjo(name):
+    if name[0] == "R" or name[0] == "r":
+         return name + " plays banjo"
+    else:
+         return name + " does not play banjo"
+    return name
