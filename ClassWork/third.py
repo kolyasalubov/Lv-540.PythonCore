42d59fa2d4e8ce1cc24239f293852a8e243c6854
Andriisas <48296925+Andriisas@users.noreply.github.com>
2020-11-22 16:31:43 +0200

    Add files via upload

---
+++ ClassWork/third.py
@@ -0,0 +1,9 @@
+List = [1,2,3,4,5,6,7,8,9]
+
+for x in List:
+    if x % 2 != 0:
+        print("There is an odd number")
+        break
+    else:
+        print("Every number is even")
+        break
