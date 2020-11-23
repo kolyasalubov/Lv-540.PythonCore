1a76ebf734f1371758a9576ebd7963400597116e
Yurii Zaverukha <niko2yurii@gmail.com>
2020-11-23 10:18:24 +0200

    add new folder HW_7 and new files

---
+++ HW_7/Zaverukha/Double Char.py
@@ -0,0 +1,5 @@
+def double_char(s):
+    char = ''
+    for item in s:
+        char += item*2
+    return char
---
+++ HW_7/Zaverukha/Grasshopper - Summation.py
@@ -0,0 +1,7 @@
+def summation(num):
+    I = 0
+    
+    for number in range (0,num+1):
+        I += number
+
+    return I
